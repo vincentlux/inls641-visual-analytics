804a8ff918046d7b8d05749d51e0fc5634923012
vincentlunc <xiaopeng@live.unc.edu>
2018-10-04 01:41:46 -0400

    Add handson5

---
+++ class_exercise/handson5/handson5.js
@@ -0,0 +1,12 @@
+var p_threshold = 0.01;
+var width = 150;
+var height = 150;
+var margin_x = 20;
+var margin_y = 20;
+
+function produceAndPositionScatterPlot(data1, data2, field1, field2) {
+	console.log("Producing and positioning " + field1 + " x " + field2);
+}
+
+function renderScatterplot(svg, field1, data1, field2, data2) {
+}
